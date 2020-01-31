b48ba3ed6ac1b53c62398ca1bf6ffb26d90c8a00
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-01-31 16:30:08 +0100

    🐛 fix share img url

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ export default {
             {
                 hid: 'og:image',
                 property: 'og:image',
-                content: '/share.png'
+                content: 'https://stereosupercopter.fr/share.png'
             },
             {
                 hid: 'og:image:width',
